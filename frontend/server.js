f74a9a6ae6430773677ab94b3f3ced64bf97ff7f
oam-victor <victor.odam@gmail.com>
2023-09-16 19:07:30 -0300

    Dockerizing

---
+++ frontend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, '../');
+
+const app = express();
+const port = 5173;
+
+// Serve static files from the 'dist' directory
+app.use(express.static(join(__dirname, 'dist')));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
