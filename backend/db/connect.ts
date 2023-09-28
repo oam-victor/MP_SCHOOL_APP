52d460a3dc7ad3475ad9265346a04584f7e7baa7
oam-victor <victor.odam@gmail.com>
2023-09-27 22:53:59 -0300

    update_docker compose

--- backend/db/connect.ts
+++ backend/db/connect.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 
-const mongodb_url = "mongodb+srv://victorodam:jo08FRn39ag4GJn4@cluster0.uam3il2.mongodb.net/?retryWrites=true&w=majority";
+const mongodb_url="mongodb+srv://victorodam:jo08FRn39ag4GJn4@cluster0.uam3il2.mongodb.net/?retryWrites=true&w=majority";
 
 export const db_connect = async () =>{
     try{
